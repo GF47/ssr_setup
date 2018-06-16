cd745f0945a518c19a9cfde0de2cbaaf0ca159b1
GF47 <liougourenmo@gmail.com>
2018-06-16 20:16:45 +0800

    add user-config.json

---
+++ ssr_ubuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# passwd root
+# narutofugen
+
+apt-get update
+apt-get install -y git python-m2crypto vim
+
+cd /usr/local
+git clone -b akkariiin/master https://github.com/shadowsocksrr/shadowsocksr.git
+cd shadowsocksr
+bash initcfg.sh
+
+mv user-config.json user-config.json.backup
+wget --no-check-certificate https://github.com/GF47/ssr_setup/user-config.json
+chmod 664 user-config.json
+wget --no-check-certificate https://github.com/Ellean/across/raw/master/bbr.sh
+chmod +x bbr.sh
+sh bbr.sh
+
+cd /usr/local/shadowsocksr/shadowsocks
+python server.py
