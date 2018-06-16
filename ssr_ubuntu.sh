ec7f20f9c83248ccf52bb2ef09e0862ad7313795
GF47 <liougourenmo@gmail.com>
2018-06-16 20:33:00 +0800

    sh bbr.sh to ./bbr.sh

--- ssr_ubuntu.sh
+++ ssr_ubuntu.sh
@@ -12,11 +12,11 @@ cd shadowsocksr
 bash initcfg.sh
 
 mv user-config.json user-config.json.backup
-wget --no-check-certificate https://github.com/GF47/ssr_setup/user-config.json
+wget --no-check-certificate https://github.com/GF47/ssr_setup/raw/master/user-config.json
 chmod 664 user-config.json
 wget --no-check-certificate https://github.com/Ellean/across/raw/master/bbr.sh
 chmod +x bbr.sh
-sh bbr.sh
+./bbr.sh
 
 cd /usr/local/shadowsocksr/shadowsocks
 python server.py
